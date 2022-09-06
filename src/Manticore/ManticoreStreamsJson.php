1e86d70d5e8777c7df5b300994d90554d45abd43
djklim87 <djklim87@gmail.com>
2022-09-06 19:09:55 +0300

    Metadata fix

--- src/Manticore/ManticoreStreamsJson.php
+++ src/Manticore/ManticoreStreamsJson.php
@@ -15,7 +15,13 @@ class ManticoreStreamsJson extends ManticoreJson
         parent::__construct($clusterName, $binaryPort);
 
         $this->notification = $notification;
-        Analog::log('Tests metadata ' . file_get_contents('/var/lib/manticore/tests/tests.meta'));
+        $testsIndexMetadata = '/var/lib/manticore/tests/tests.meta';
+        if (file_exists($testsIndexMetadata)){
+            Analog::log('Tests metadata ' . file_get_contents($testsIndexMetadata));
+        }else{
+            Analog::log('Tests metadata not found');
+        }
+
         $this->restoreExistingIndexes();
     }
 
